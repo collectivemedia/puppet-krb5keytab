0c0dbd96daeda4e4e9b284d2417d6dcf116d4009
Sarguru Nathan <smohan@collective.com>
2014-10-01 23:52:20 +0530

    refactor a bit to make it easy to test and have proper variable scope

--- manifests/host_keytab.pp
+++ manifests/host_keytab.pp
@@ -24,33 +24,27 @@ class krb5keytab::host_keytab inherits krb5keytab {
   # Some parameters are needed by the script
   #
 
-  $admin_keytab = hiera('krb5keytab::admin-keytab', '*undefined*')
-  if ($admin_keytab == '*undefined*') {
+  if ($krb5keytab::admin_keytab == '*undefined*') {
     fail "Missing parameter 'krb5keytab::admin-keytab' (base64 encoded kerberos admin credential) is not defined in hiera"
   }
 
-  $admin_princ = hiera('krb5keytab::admin-principal', '*undefined*')
-  if ($admin_princ == '*undefined*') {
+  if ($krb5keytab::admin_princ == '*undefined*') {
     fail "Missing parameter 'krb5keytab::admin-principal' (name of the kerberos admin principal) is not defined in hiera"
   }
 
-  $krb5_realm = hiera('krb5keytab::krb5-realm', '*undefined*')
-  if ($krb5_realm == '*undefined*') {
+  if ($krb5keytab::krb5_realm == '*undefined*') {
     fail "Missing parameter 'krb5keytab::krb5-realm' (Kerberos realm) is not defined in hiera"
   }
 
-  $hiera_backend = hiera('krb5keytab::hiera-backend', '*undefined*')
-  if ($hiera_backend == '*undefined*') {
+  if ($krb5keytab::hiera_backend == '*undefined*') {
     fail "Missing parameter 'krb5keytab::hiera-backend' (name of backend script to run) is not defined in hiera"
   }
 
-  $ldap_ou = hiera('krb5keytab::ldap-ou', '*undefined*')
-  if ($ldap_ou == '*undefined*') {
+  if ($krb5keytab::ldap_ou == '*undefined*') {
     fail "Missing parameter 'krb5keytab::ldap-ou' (where to put host keys in LDAP) is not defined in hiera"
   }
 
-  $krb5_admin_server = hiera('krb5keytab::krb5-admin-server', '*undefined*')
-  if ($krb5_admin_server == '*undefined*') {
+  if ($krb5keytab::krb5_admin_server == '*undefined*') {
     fail "Missing parameter 'krb5keytab::krb5-admin-server' (FQDN and optionally port of kerberos admin server) is not defined in hiera"
   }
 
@@ -59,41 +53,40 @@ class krb5keytab::host_keytab inherits krb5keytab {
   # as base64 encoded. We will assume that hiera accepts the keytab as base64
   # encoded too.
   #
-
-  $h_keytab = hiera('krb5-keytab', '*undefined*')
-  if ($h_keytab == '*undefined*') {
+  
+  if ($krb5keytab::h_keytab == '*undefined*') {
 
     # Store the keytab contents in a file on the server and pass in the
     # argument as a filename. Otherwise if there's an error the puppet agent might
     # be able to see the key in the error message, and that would be bad!
 
-    $admin_keytab_file_path = krb5keytab_writefile(base64('decode',$admin_keytab))
+    $admin_keytab_file_path = krb5keytab_writefile(base64('decode',$krb5keytab::admin_keytab))
 
     #
     # Get the host keytab from the Kerberos server. This calls the
     # lib/puppet/parser/functions/krb5keytab_generatekt.rb file in this module.
     #
-    
+
     $keytab = krb5keytab_generatekt( {
       admin_keytab => $admin_keytab_file_path,
-      admin_principal => $admin_princ,
-      realm => $krb5_realm,
-      ldap_ou => $ldap_ou,
-      admin_server => $krb5_admin_server,
+      admin_principal => $krb5keytab::admin_princ,
+      realm => $krb5keytab::krb5_realm,
+      ldap_ou => $krb5keytab::ldap_ou,
+      admin_server => $krb5keytab::krb5_admin_server,
       fqdn => $::fqdn
     } )
-    
+
     #
     # Store the host keytab in your hiera database, so it doesn't have to get
     # regenerated the next time the puppet runs for this host. This calls the
     # lib/puppet/parser/functions/krb5keytab_saveinhiera.rb file in this module.
     #
-    
+
     krb5keytab_saveinhiera( {
       hiera_key => 'krb5-keytab',
       hiera_value => base64('encode', $keytab),
       fqdn => $::fqdn,
-      hiera_backend => $hiera_backend,
+      hiera_backend => $krb5keytab::hiera_backend,
       hiera_file_dir => hiera('krb5keytab::hiera-file-dir', ''),
       hiera_couchdb_hostname => hiera('krb5keytab::hiera-couchdb-hostname', '127.0.0.1'),
       hiera_couchdb_port => hiera('krb5keytab::hiera-couchdb-port', '5984'),
@@ -103,10 +96,10 @@ class krb5keytab::host_keytab inherits krb5keytab {
     } )
 
   } else {
-  
+
     ## Don't break with some legacy encoding from earlier versions
-    $h_keytab_repl = regsubst($h_keytab, '^BASE64:<(.+)>$', '\1', 'M')
-  
+    $h_keytab_repl = regsubst($krb5keytab::h_keytab, '^BASE64:<(.+)>$', '\1', 'M')
+
     ## Decode the keytab
     $keytab = base64('decode', $h_keytab_repl)
   }
