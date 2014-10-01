b52c15b9ce1b77c02f0fa94d735db0bb58c63cae
Sarguru Nathan <smohan@collective.com>
2014-10-01 23:52:40 +0530

    test class

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 describe 'krb5keytab' do
 
-  context 'with defaults for all parameters' do
+  context 'with given parameters' do
+    let(:params) {{:admin_keytab => 'test',:admin_princ => 'test',
+                   :krb5_realm => 'test', :hiera_backend => 'test',
+                   :krb5_admin_server => 'test', :ldap_ou => 'test',
+                   :h_keytab => 'test' }}
     it { should contain_class('krb5keytab') }
+    it { should contain_file('krb5-host-keytab')}
+    it { should contain_class('krb5keytab::host_keytab')}
   end
 end
